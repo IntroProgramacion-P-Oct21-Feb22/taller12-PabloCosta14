64db4029d1ca46bb088de6404573d5eac49c9911
Pablo Costa <pacosta2@utpl.edu.ec>
2022-01-23 14:10:43 -0500

    commit

--- Ejercicios/src/ejercicios/Problema01.java
+++ Ejercicios/src/ejercicios/Problema01.java
@@ -16,6 +16,27 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        double[][] resultado = new double[4][2];
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                resultado[i][j] = factorial(datos[i][j]);
+                System.out.printf("%.2f\n", resultado[i][j]);
+            }
+
+        }
+
     }
-    
+
+    public static double factorial(double datos) {
+
+        if (datos <= 1) {
+            return 1;
+
+        } else {
+
+            return datos * factorial(datos - 1);
+        }
+    }
+
 }
--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -15,7 +15,32 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[] bases = {100, 90, 80, 70, 60};
+        double[] potencias = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+        for (int i = 0; i < bases.length; i++) {
+            resultado[i] = potencia(bases[i], potencias[i]);
+
+        }
+        System.out.printf("%s\n", suma(resultado, resultado.length));
     }
-    
+
+    public static double potencia(double base, double exponente) {
+
+        if (exponente == 1) {
+            return base;
+        } else {
+
+            return base * potencia(base, exponente - 1);
+        }
+    }
+
+    public static double suma(double[] arreglo, int tamanio) {
+
+        if (tamanio == 1) {
+            return arreglo[0];
+        } else {
+            return arreglo[tamanio - 1] + suma(arreglo, tamanio - 1);
+        }
+    }
 }
